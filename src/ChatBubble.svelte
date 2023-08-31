679b0e9206c8d4ffb267b95b16b87b6100142d67
Jack Schofield <59843981+jschof1@users.noreply.github.com>
2023-08-31 11:21:15 +0100

    first push

--- src/ChatBubble.svelte
+++ src/ChatBubble.svelte
@@ -1,5 +1,6 @@
 <script>
   import { onMount } from 'svelte';
+  import { fade } from 'svelte/transition'
   export let message = "";
   export let onAppear = () => {};
   
--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import './app.css'
-import App from './App.svelte'
+// Your existing code
+import './app.css';
+import App from './App.svelte';
 
 const app = new App({
-  target: document.body
-})
+  target: document.getElementById('app')
+});
 
-export default app
+export default app;
