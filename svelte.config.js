765dcaba1acf27fdbe3db013e349b1d1574d3ec2
Jack Schofield <59843981+jschof1@users.noreply.github.com>
2023-10-06 14:11:41 +0100

    update config for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -4,8 +4,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: '/consequence-scanning-activity/', // correct path for GitHub Pages
   build: {
-    outDir: './docs', // relative to index.html
-    // emptyOutDir: true, // true if outDir is inside root. if outDir is not inside root, uncomment this.
+    outDir: './docs', // output directory
+    // emptyOutDir: true, // uncomment if outDir is inside root (if needed)
   }
 })
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: 'https://github.com/jschof1/consequence-scanning-activity/',  // replace <REPO> with your repo name
+  base: '/consequence-scanning-activity/', // correct path for GitHub Pages
+  build: {
+    outDir: './docs', // output directory
+    // emptyOutDir: true, // uncomment if outDir is inside root (if needed)
+  }
 })
