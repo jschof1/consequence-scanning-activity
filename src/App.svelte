e84c024a85b6db7ef7f8d14dda3cb53786570d72
Jack Schofield <59843981+jschof1@users.noreply.github.com>
2023-11-03 11:40:14 +0000

    add demo banner

--- src/App.svelte
+++ src/App.svelte
@@ -7,12 +7,14 @@
   let src = "./odi-logo.png";
   let arrow = "./icons_arrow.svg";
 
-  let showModal = false;
-  function toggleModal() {
-    showModal = !showModal;
-  }
   </script>
 <Navbar />
+  <div class="absolute right-0 top-0 h-16 w-16">
+    <div
+      class="absolute transform rotate-45 bg-white text-center text-black font-semibold py-1 right-[-35px] top-[32px] w-[170px]">
+      DEMO
+    </div>
+  </div>
 <div class="ml-64 bg-blue-800">
   <div class="text-5xl font-bold py-16 text-center">
     <span class="text-white">Consequence and Risk Evaluation (CARE)</span>
